7a110f79861a7304fbfa3623444390e03759ee42
charlotte <3289199052@qq.com>
2026-09-19 13:52:55 +0800

    Day2:两数之和HashMap解法

---
+++ src/leetcode_TwoSum_hashmap.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+public class leetcode_TowSum_hashmap {
+    public static void main(String[] args){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int[] nums={1,2,3,4,5,6,7,8,9,10};
+        int target=18;
+        for(int i=0;i<nums.length;i++){
+            int need=target-nums[i];
+            if(map.containsKey(need)){
+                System.out.print(map.get(need)+" "+i);
+            }
+            map.put(nums[i],i);
+        }
+    }
+}

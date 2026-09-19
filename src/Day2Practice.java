bdc450530290ac83fa6889bf40bcbdb74f1719f6
charlotte <3289199052@qq.com>
2026-09-19 21:51:07 +0800

    Day2:完成九九乘法表和猜数游戏

--- src/Day2Practice.java
+++ src/Day2Practice.java
@@ -1,5 +1,12 @@
+import java.util.*;
+import java.util.Random;
 public class Day2Practice {
     public static void main(String[] args){
+
+        //printMultiplicationTable();
+        gussNumberGame();
+    }
+    public static void printMultiplicationTable(){
         for(int i=1;i<=9;i++){
             for(int j=i;j<=9;j++){
                 System.out.print(i+"*"+j+"="+i*j+"\t");
@@ -7,4 +14,28 @@ public class Day2Practice {
             System.out.println();
         }
     }
+    public static void gussNumberGame(){
+        Random rand = new Random();
+        int target=rand.nextInt(100)+1;
+        Scanner sc=new Scanner(System.in);
+        int count=0;
+        System.out.println("猜谜游戏开始，请输入数字");
+        while(true){
+            System.out.print("请输入你猜的数字");
+            int guss=sc.nextInt();
+            count++;
+            if(guss>target){
+                System.out.println("大了");
+
+            }else if(guss<target){
+                System.out.print("小了");
+
+            }else{
+                System.out.print("恭喜找到，一共花了"+count+"找到");
+                break;
+            }
+        }
+
+    }
+
 }
